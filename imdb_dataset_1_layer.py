12d5384bf4dc740d50c765e5345c7bb22d5dd270
Marius Brad <mbrad26@gmail.com>
2018-10-19 19:08:38 +0100

    ...

--- imdb_dataset_1_layer.py
+++ imdb_dataset_1_layer.py
@@ -3,11 +3,6 @@ import matplotlib.pyplot as plt
 from keras.datasets import imdb
 from keras import models, layers
 
-# word_index = imdb.get_word_index()
-# reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-# def decoded(alist):
-#     return ' '.join([reverse_word_index.get(i, '?') for i in alist])
-
 
 def vectorize(alist, dim=10000):
     a = np.zeros((len(alist), dim))
@@ -24,23 +19,24 @@ y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(train_labels).astype('float32')
 
 x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
+partial_x_val = x_train[10000:]
 y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
+partial_y_val = y_train[10000:]
+
 
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(
     optimizer='rmsprop',
     loss='binary_crossentropy',
-    metrics=['accuracy'])
+    metrics=['accuracy']
+)
 
 history = model.fit(
-    partial_x_train,
-    partial_y_train,
+    partial_x_val,
+    partial_y_val,
     epochs=4,
     batch_size=512,
     validation_data=(x_val, y_val))
@@ -72,51 +68,3 @@ plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- imdb_dataset_3_layers.py
+++ imdb_dataset_3_layers.py
@@ -3,13 +3,8 @@ import matplotlib.pyplot as plt
 from keras.datasets import imdb
 from keras import models, layers
 
-# word_index = imdb.get_word_index()
-# reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-# def decoded(alist):
-#     return ' '.join([reverse_word_index.get(i, '?') for i in alist])
 
-
-def vectorize(alist, dim=10000):
+def vectorized(alist, dim=10000):
     a = np.zeros((len(alist), dim))
     for k, v in enumerate(alist):
         a[k, v] = 1
@@ -18,45 +13,48 @@ def vectorize(alist, dim=10000):
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
-x_train = vectorize(train_data)
-x_test = vectorize(test_data)
+x_train = vectorized(train_data)
+x_test = vectorized(test_data)
 y_train = np.asarray(train_labels).astype('float32')
-y_test = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
 
 x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
+partial_x_val = x_train[10000:]
 y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
+partial_y_val = y_train[10000:]
 
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(
     optimizer='rmsprop',
     loss='binary_crossentropy',
-    metrics=['accuracy'])
+    metrics=['accuracy']
+)
 
 history = model.fit(
-    partial_x_train,
-    partial_y_train,
-    epochs=4,
+    partial_x_val,
+    partial_y_val,
+    epochs=20,
     batch_size=512,
-    validation_data=(x_val, y_val))
+    validation_data=(x_val, y_val)
+)
 
 results = model.evaluate(x_test, y_test)
-print(results)
 
 
 history_dict = history.history
-loss_values = history_dict['loss']
+loss = history_dict['loss']
+acc = history_dict['acc']
 val_loss = history_dict['val_loss']
-acc_values = history_dict['acc']
 val_acc = history_dict['val_acc']
-epochs = range(1, len(acc_values) + 1)
 
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.xlabel('Epochs')
@@ -64,25 +62,14 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
-
-plt.plot(epochs, acc_values, 'bo', label='Training loss')
-plt.plot(epochs, val_acc, 'b', label='Validation loss')
+plt.clf()
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
 plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
 
 
 
