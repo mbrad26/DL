5a83bd741fc3cb0c23f0daef09713902f45ce606
Marius Brad <mbrad26@gmail.com>
2018-10-22 16:17:33 +0100

    ...

--- reuters_v2_dataset.py
+++ reuters_v2_dataset.py
@@ -26,7 +26,6 @@ partial_y_val = y_train[1000:]
 
 model = models.Sequential()
 model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(46, activation='softmax'))
 
 model.compile(
